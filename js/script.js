c360360d46f526ec67a7194360bf1117fa6e57d9
Sônia Lopes Rodrigues <sonialopesrodriges@gmail.com>
2026-03-27 16:21:26 -0300

    Alteração e testes na função de salvar

--- js/script.js
+++ js/script.js
@@ -11,45 +11,68 @@ function login() {
         document.getElementById("erro").style.display = "block";        
     }
 }
-const btnSalvar= document.getElementById('btnSalvar')
+const btnSalvar= document.getElementById('btnSalvar');
 const form = document.getElementById('formCliente');
+const tpTerceiro= document.getElementById('tpTerceiro');
+const uf=document.getElementById('uf');
+const razao=document.getElementById('nomeCliente');
+let tipoTerceiro="";
+let optUf="";
+
+
+tpTerceiro.addEventListener('change', (e) => {
+    tipoTerceiro = e.target.value;    
+});
+ 
+uf.addEventListener('change', (e) => {
+    optUf = e.target.value;    
+});
+
 
 
 // Captura os eventos do formulário Clientes
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-
     
-btnSalvar.addEventListener('click', (e) => {    
-    e.preventDefault();
+    
+    btnSalvar.addEventListener('click', (e) => {    
+             e.preventDefault();
 
-// Captura os valores
-const novoCliente = {
-    nome: document.getElementById('nomeCliente').value,
-    rua: document.getElementById('rua').value,
-    email: document.getElementById('email').value,
-    numero: document.getElementById('numero').value,
-    cep: document.getElementById('cep').value,
-    cidade: document.getElementById('cidade').value,
-    bairro: document.getElementById('bairro').value,
-    cpf: document.getElementById('cpf').value,
-    cnpj: document.getElementById('cnpj').value,
-    inscEstadual: document.getElementById('inscEstadual').value,
-    fone: document.getElementById('fone').value,
-    celular: document.getElementById('celular').value,
-    responsavel: document.getElementById('resp').value,
-    aniver: document.getElementById('aniver').value,
-    site: document.getElementById('site').value
-};
-      
-    const clientes = JSON.parse(localStorage.getItem('clientes')) || [];
-    clientes.push(novoCliente);
-    localStorage.setItem('clientes', JSON.stringify(clientes));
+        //TESTA PARA VER SE O NOME DO CLIENTE FOI Informado
+        console.log("o nome do xliente " + razao)
+        if (razao.value===""){
+            alert ("Informe o Nome/Razão Social")
+            return
+        }     
+        // Captura os valores
+        const novoCliente = {
+            nome: document.getElementById('nomeCliente').value,
+            rua: document.getElementById('rua').value,
+            email: document.getElementById('email').value,
+            numero: document.getElementById('numero').value,
+            cep: document.getElementById('cep').value,
+            cidade: document.getElementById('cidade').value,
+            bairro: document.getElementById('bairro').value,
+            cpf: document.getElementById('cpf').value,
+            cnpj: document.getElementById('cnpj').value,
+            inscEstadual: document.getElementById('inscEstadual').value,
+            fone: document.getElementById('fone').value,
+            celular: document.getElementById('celular').value,
+            responsavel: document.getElementById('resp').value,
+            aniver: document.getElementById('aniver').value,
+            site: document.getElementById('site').value,
+            tpTerceUro: tipoTerceiro,
+            uf: optUf,
+        };
+
+            const clientes = JSON.parse(localStorage.getItem('clientes')) || [];
+            clientes.push(novoCliente);
+            localStorage.setItem('clientes', JSON.stringify(clientes));
 
 
-    form.reset();
-    alert('Salvo com sucesso!');
-  
-});
-})
+            form.reset();
+            alert('Salvo com sucesso!');
+
+        });
+})  
 
